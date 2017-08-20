c52d9ed773ccaceeeeed3838f7a79f054ad2d6de
David Sanders <david.wm.sanders@gmail.com>
2017-08-20 02:33:55 +0100

    Add existing source

---
+++ stats.py
@@ -0,0 +1,32 @@
+import collections
+import csv
+from pathlib import Path
+
+csv_path = Path("stats.csv")
+Stats = collections.namedtuple("Stats",
+"utc time_played game_version kills deaths team_kills killstreak " \
+"targets_destroyed vehicles_destroyed soldiers_healed distance_moved " \
+"shots_fired throwables_thrown rank_progression deathstreak kill_combos")
+
+def load_stats():
+    stats = []
+    with csv_path.open("r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        for r in reader:
+            s = Stats(r["utc"], int(r["tp"]), int(r["gv"]), int(r["k"]),
+                      int(r["d"]), int(r["tk"]), int(r["ks"]), int(r["td"]),
+                      int(r["vd"]), int(r["sh"]), float(r["dm"]), int(r["sf"]),
+                      int(r["tt"]), float(r["rp"]), int(r["ds"]), r["kc"])
+            stats.append(s)
+    return stats
+
+
+if __name__ == '__main__':
+    stats = load_stats()
+    stats = stats[-1]
+    len_stats = len(stats)
+    print(f"There are {len_stats} rows of stats.")
+    tp_hrs = stats.time_played/3600
+    kdr = stats.kills / stats.deaths
+    print(f"Latest: Time played: {tp_hrs:.2f} hours, Kills: {stats.kills}, " \
+          f"Deaths: {stats.deaths}, KDR: {kdr:.2f}")
---
+++ track.py
@@ -0,0 +1,75 @@
+import csv
+import xml.etree.ElementTree as etree
+from datetime import datetime, timezone
+from pathlib import Path
+
+csv_path = Path("stats.csv")
+profile_path = Path.home() / "AppData\Roaming\Running with rifles\summary_profile.xml"
+
+field_headers = ["utc", "tp", "gv", "k", "d", "tk", "ks", "td", "vd", "sh", "dm", "sf", "tt", "rp", "ds", "kc"]
+
+if not csv_path.exists():
+    with csv_path.open("w", newline="") as csv_file:
+        print("Creating stats.csv and writing CSV field headers...")
+        writer = csv.writer(csv_file)
+        writer.writerow(field_headers)
+
+with profile_path.open() as profile_file:
+    print("Processing summary_profile.xml...")
+    profile_xml = profile_file.read()
+    profile = etree.fromstring(profile_xml)
+    stats_element = profile.find("stats")
+    stats = stats_element.attrib
+    utc = datetime.now(timezone.utc)
+    tp = int(float(stats["time_played"]))
+    gv = profile.attrib["game_version"]
+    k = stats["kills"]
+    d = stats["deaths"]
+    tk = stats["teamkills"]
+    ks = stats["longest_kill_streak"]
+    td = stats["targets_destroyed"]
+    vd = stats["vehicles_destroyed"]
+    sh = stats["soldiers_healed"]
+    dm = stats["distance_moved"]
+    sf = stats["shots_fired"]
+    tt = stats["throwables_thrown"]
+    rp = stats["rank_progression"]
+    ds = -1
+    kc = -1
+    monitor_elements = stats_element.findall("monitor")
+    for monitor_element in monitor_elements:
+        monitor = monitor_element.attrib
+        if "name" in monitor:
+            if monitor["name"] == "death streak":
+                ds = monitor["longest_death_streak"]
+            elif monitor["name"] == "kill combo":
+                kill_combos_d = {}
+                entry_elements = monitor_element.findall("entry")
+                for entry_element in entry_elements:
+                    entry = entry_element.attrib
+                    kill_combos_d[int(entry["combo"])] = int(entry["count"])
+                kill_combos = []
+                x = 3
+                while True:
+                    if x in kill_combos_d:
+                        c = kill_combos_d[x]
+                        kill_combos.append(f"{x}:{c}")
+                        x += 1
+                    else:
+                        break
+                kc = ",".join(kill_combos)
+            else:
+                print(f"""Monitor '{monitor["name"]}' not processed.""")
+
+    # print(f"utc={utc}, tp={tp}, gv={gv}, k={k}, d={d}, tk={tk}, ks={ks}, td={td}, vd={vd}, sh={sh}, dm={dm}, sf={sf}, tt={tt}, rp={rp}, ds={ds}, kc={kc}")
+
+    with csv_path.open("r+", newline="") as csv_file:
+        csvd = csv_file.readlines()
+        last_row = csvd[-1]
+        previous_tp = last_row.split(",")[1]
+        if previous_tp == "tp" or tp > int(previous_tp):
+            print("Updating stats.csv")
+            writer = csv.writer(csv_file)
+            writer.writerow([utc, tp, gv, k, d, tk, ks, td, vd, sh, dm, sf, tt, rp, ds, kc])
+        elif tp == int(previous_tp):
+            print("No change to time played in summary_profile.xml since stats.csv was last updated")
