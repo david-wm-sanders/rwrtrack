0f2f6507b9c53314e2b1cbb5db85158a4c884658
David Sanders <david.wm.sanders@gmail.com>
2017-09-20 21:35:05 +0100

    Rename rwrtrack.py to get_stats.py

--- pep8_rwrtrack.py
+++ pep8_rwrtrack.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 # Run pep8 --exclude=venv --show-source .\source.py
-files = ["rwrtrack.py", "stats.py", "sums.py"]
+files = ["stats.py", "get_stats.py", "sums.py"]
 for f in files:
     p = Path(__file__).parent / Path(f)
     try:
