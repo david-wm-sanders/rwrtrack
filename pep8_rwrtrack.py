e7db363c311896cd8246cc0910a42d4774e269fa
David Sanders <david.wm.sanders@gmail.com>
2017-09-26 23:41:02 +0100

    Simplify pep8_rwrtrack.py

--- pep8_rwrtrack.py
+++ pep8_rwrtrack.py
@@ -2,8 +2,6 @@ import subprocess
 from pathlib import Path
 
 
-# Run pep8 --exclude=venv --show-source .\source.py
-files = ["stats.py", "get_stats.py", "sums.py", "analysis.py", "ranking.py"]
-for f in files:
-    p = Path(__file__).parent / Path(f)
-    subprocess.run(["pep8", "--exclude=venv","--show-source", str(p)])
+# Run pep8 --exclude=venv,offline --show-source .
+p = Path(__file__).parent / Path(".")
+subprocess.run(["pep8", "--exclude=venv,offline", "--show-source", str(p)])
