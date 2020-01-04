3ecbeade659c8c05d56bfe80965592e5ede0e406
David Sanders <david.wm.sanders@gmail.com>
2020-01-04 04:30:03 +0000

    Hybridise all derived statistic properties

--- rwrtrack/core/derivedstats.py
+++ rwrtrack/core/derivedstats.py
@@ -5,12 +5,13 @@ from .constants import earth_equatorial_circumference
 
 
 class DerivedStats:
-    """Implements derived statistics that are not stored in columns in the Records table"""
-
+    """Implements derived statistics"""
+    # Score
     @hybrid_property
     def score(self):
         return self.kills - self.deaths
 
+    # K/D ratio
     @hybrid_property
     def kdr(self):
         try:
@@ -18,125 +19,201 @@ class DerivedStats:
         except ZeroDivisionError:
             return self.kills
 
-    # Special expression case if using "kdr" directly in SQLAlchemy queries
-    # This effectively mirrors the logic for the property above but as something that can be translated to SQL
     @kdr.expression
     def kdr(cls):
-        return case([(cls.deaths > 0, cls.kills / cls.deaths)], else_ = cls.kills)
+        return case([(cls.deaths > 0, cls.kills / cls.deaths)], else_=cls.kills)
 
-    @property
+    # Time played in hours
+    @hybrid_property
     def time_played_hours(self):
         return self.time_played / 60
 
-    @property
+    # Distance moved in km
+    @hybrid_property
     def distance_moved_km(self):
         return self.distance_moved / 1000
 
-    @property
+    # XP per hour
+    @hybrid_property
     def xp_per_hour(self):
         try:
             return self.xp / self.time_played_hours
         except ZeroDivisionError:
             return 0
 
-    @property
+    @xp_per_hour.expression
+    def xp_per_hour(cls):
+        return case([(cls.time_played > 0, cls.xp / cls.time_played_hours)], else_=0)
+
+    # Kills per hour
+    @hybrid_property
     def kills_per_hour(self):
         try:
             return self.kills / self.time_played_hours
         except ZeroDivisionError:
             return 0
 
-    @property
+    @kills_per_hour.expression
+    def kills_per_hour(cls):
+        return case([(cls.time_played > 0, cls.kills / cls.time_played_hours)], else_=0)
+
+    # Deaths per hour
+    @hybrid_property
     def deaths_per_hour(self):
         try:
             return self.deaths / self.time_played_hours
         except ZeroDivisionError:
             return 0
 
-    @property
+    @deaths_per_hour.expression
+    def deaths_per_hour(cls):
+        return case([(cls.time_played > 0, cls.deaths / cls.time_played_hours)], else_=0)
+
+    # Targets destroyed per hour
+    @hybrid_property
     def targets_destroyed_per_hour(self):
         try:
             return self.targets_destroyed / self.time_played_hours
         except ZeroDivisionError:
             return 0
 
-    @property
+    @targets_destroyed_per_hour.expression
+    def targets_destroyed_per_hour(cls):
+        return case([(cls.time_played > 0, cls.targets_destroyed / cls.time_played_hours)], else_=0)
+
+    # Vehicles destroyed per hour
+    @hybrid_property
     def vehicles_destroyed_per_hour(self):
         try:
             return self.vehicles_destroyed / self.time_played_hours
         except ZeroDivisionError:
             return 0
 
-    @property
+    @vehicles_destroyed_per_hour.expression
+    def vehicles_destroyed_per_hour(cls):
+        return case([(cls.time_played > 0, cls.vehicles_destroyed / cls.time_played_hours)], else_=0)
+
+    # Soldiers healed per hour
+    @hybrid_property
     def soldiers_healed_per_hour(self):
         try:
             return self.soldiers_healed / self.time_played_hours
         except ZeroDivisionError:
             return 0
 
-    @property
+    @soldiers_healed_per_hour.expression
+    def soldiers_healed_per_hour(cls):
+        return case([(cls.time_played > 0, cls.soldiers_healed / cls.time_played_hours)], else_=0)
+
+    # Team kills per hour
+    @hybrid_property
     def team_kills_per_hour(self):
         try:
             return self.team_kills / self.time_played_hours
         except ZeroDivisionError:
             return 0
 
-    @property
+    @team_kills_per_hour.expression
+    def team_kills_per_hour(cls):
+        return case([(cls.time_played > 0, cls.team_kills / cls.time_played_hours)], else_=0)
+
+    # Distance moved (in km) per hour
+    @hybrid_property
     def distance_moved_km_per_hour(self):
         try:
             return self.distance_moved_km / self.time_played_hours
         except ZeroDivisionError:
             return 0
 
-    @property
+    @distance_moved_km_per_hour.expression
+    def distance_moved_km_per_hour(cls):
+        return case([(cls.time_played > 0, cls.distance_moved_km / cls.time_played_hours)], else_=0)
+
+    # Shots fired per hour
+    @hybrid_property
     def shots_fired_per_hour(self):
         try:
             return self.shots_fired / self.time_played_hours
         except ZeroDivisionError:
             return 0
 
-    @property
+    @shots_fired_per_hour.expression
+    def shots_fired_per_hour(cls):
+        return case([(cls.time_played > 0, cls.shots_fired / cls.time_played_hours)], else_=0)
+
+    # Throwables thrown per hour
+    @hybrid_property
     def throwables_thrown_per_hour(self):
         try:
             return self.throwables_thrown / self.time_played_hours
         except ZeroDivisionError:
             return 0
 
-    @property
+    @throwables_thrown_per_hour.expression
+    def throwables_thrown_per_hour(cls):
+        return case([(cls.time_played > 0, cls.throwables_thrown / cls.time_played_hours)], else_=0)
+
+    # Kills per km moved
+    @hybrid_property
     def kills_per_km_moved(self):
         try:
             return self.kills / self.distance_moved_km
         except ZeroDivisionError:
             return 0
 
-    @property
+    @kills_per_km_moved.expression
+    def kills_per_km_moved(cls):
+        return case([(cls.distance_moved > 0, cls.kills / cls.distance_moved_km)], else_=0)
+
+    # XP per shot fired
+    @hybrid_property
     def xp_per_shot_fired(self):
         try:
             return self.xp / self.shots_fired
         except ZeroDivisionError:
             return 0
 
-    @property
+    @xp_per_shot_fired.expression
+    def xp_per_shot_fired(cls):
+        return case([(cls.shots_fired > 0, cls.xp / cls.shots_fired)], else_=0)
+
+    # XP per kill
+    @hybrid_property
     def xp_per_kill(self):
         try:
             return self.xp / self.kills
         except ZeroDivisionError:
             return 0
 
-    @property
+    @xp_per_kill.expression
+    def xp_per_kill(cls):
+        return case([(cls.kills > 0, cls.xp / cls.kills)], else_=0)
+
+    # Shots fired per kill
+    @hybrid_property
     def shots_fired_per_kill(self):
         try:
             return self.shots_fired / self.kills
         except ZeroDivisionError:
             return 0
 
-    @property
+    @shots_fired_per_kill.expression
+    def shots_fired_per_kill(cls):
+        return case([(cls.kills > 0, cls.shots_fired / cls.kills)], else_=0)
+
+    # Team kills per kill
+    @hybrid_property
     def team_kills_per_kill(self):
         try:
             return self.team_kills / self.kills
         except ZeroDivisionError:
             return 0
 
-    @property
+    @team_kills_per_kill.expression
+    def team_kills_per_kill(cls):
+        return case([(cls.kills > 0, cls.team_kills / cls.kills)], else_=0)
+
+    # Runs around the Earth equator
+    @hybrid_property
     def runs_around_the_equator(self):
         return self.distance_moved_km / earth_equatorial_circumference
