706ad91fd6eca62f90c0b85d049e8d34f41647b8
David Sanders <david.wm.sanders@gmail.com>
2020-01-10 02:08:21 +0000

    Add diffsum

--- rwrtrack/core/sum.py
+++ rwrtrack/core/sum.py
@@ -1,8 +1,9 @@
-from sqlalchemy.sql import func
+from sqlalchemy.sql import func, and_
 from sqlalchemy.orm.query import Query
 
 from .db import sesh
-from .record import Record
+from .record import Record, RA, RB
+from .difference import Diff
 from .constants import EARTH_EQUAT_CIRC
 
 
@@ -23,8 +24,33 @@ class Sum:
     throwables_thrown = func.sum(Record.throwables_thrown).label("throwables_thrown")
 
     # Converted statistics
-    time_played_hours = (func.sum(Record.time_played) / 60.0).label("time_played_hours")
-    distance_moved_km = (func.sum(Record.distance_moved) / 1000.0).label("distance_moved_km")
+    time_played_hours = (time_played / 60.0).label("time_played_hours")
+    distance_moved_km = (distance_moved / 1000.0).label("distance_moved_km")
+
+    # Derived statistics
+    score = (kills - deaths).label("score")
+    runs_around_the_equator = (distance_moved_km / EARTH_EQUAT_CIRC).label("runs_around_the_equator")
+
+
+class DiffSum:
+    _count = func.count(RA.account_id).label("_count")
+    # Columned statistics
+    xp = func.sum(Diff.xp).label("xp")
+    time_played = func.sum(Diff.time_played).label("time_played")
+    kills = func.sum(Diff.kills).label("kills")
+    deaths = func.sum(Diff.deaths).label("deaths")
+    kill_streak = func.sum(Diff.kill_streak).label("kill_streak")
+    targets_destroyed = func.sum(Diff.targets_destroyed).label("targets_destroyed")
+    vehicles_destroyed = func.sum(Diff.vehicles_destroyed).label("vehicles_destroyed")
+    soldiers_healed = func.sum(Diff.soldiers_healed).label("soldiers_healed")
+    team_kills = func.sum(Diff.team_kills).label("team_kills")
+    distance_moved = func.sum(Diff.distance_moved).label("distance_moved")
+    shots_fired = func.sum(Diff.shots_fired).label("shots_fired")
+    throwables_thrown = func.sum(Diff.throwables_thrown).label("throwables_thrown")
+
+    # Converted statistics
+    time_played_hours = (time_played / 60.0).label("time_played_hours")
+    distance_moved_km = (distance_moved / 1000.0).label("distance_moved_km")
 
     # Derived statistics
     score = (kills - deaths).label("score")
@@ -36,9 +62,23 @@ sum_query = Query([Sum._count, Sum.xp, Sum.time_played, Sum.time_played_hours, S
                    Sum.team_kills, Sum.distance_moved, Sum.distance_moved_km,
                    Sum.shots_fired, Sum.throwables_thrown, Sum.runs_around_the_equator])
 
+diffsum_query = Query([DiffSum._count, DiffSum.xp, DiffSum.time_played, DiffSum.time_played_hours,
+                       DiffSum.kills, DiffSum.deaths, DiffSum.score, DiffSum.kill_streak,
+                       DiffSum.targets_destroyed, DiffSum.vehicles_destroyed, DiffSum.soldiers_healed,
+                       DiffSum.team_kills, DiffSum.distance_moved, DiffSum.distance_moved_km,
+                       DiffSum.shots_fired, DiffSum.throwables_thrown, DiffSum.runs_around_the_equator]).\
+                       filter(RA.account_id==RB.account_id)
+
 
 def sum_(date, usernames=None):
     q = sum_query.with_session(sesh).filter(Record.date==date)
     if usernames:
         q = q.filter(Record.username.in_(usernames))
-    return q
+    return q.one()._asdict()
+
+
+def diffsum(date_a, date_b, usernames=None):
+    q = diffsum_query.with_session(sesh).filter(and_(RA.date==date_a, RB.date==date_b))
+    if usernames:
+        q = q.filter(RA.username.in_(usernames))
+    return q.one()._asdict()
