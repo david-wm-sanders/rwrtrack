e8988f1dcf9875b03ee3eb10850afc681686c393
David Sanders <david.wm.sanders@gmail.com>
2020-01-15 09:29:11 +0000

    Simplify DbInfo

--- rwrtrack/core/dbinfo.py
+++ rwrtrack/core/dbinfo.py
@@ -1,7 +1,6 @@
 import logging
 
 from sqlalchemy import Column, Integer
-from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm.exc import NoResultFound
 
 from .db import DeclarativeBase, sesh
@@ -13,28 +12,15 @@ logger = logging.getLogger(__name__)
 class DbInfo(DeclarativeBase):
     __tablename__ = "_dbinfo"
     _id = Column(Integer, primary_key=True)
-    _first_date = Column("first_date", Integer, nullable=False)
-    _latest_date = Column("latest_date", Integer, nullable=False)
+    first_date = Column(Integer, nullable=False)
+    latest_date = Column(Integer, nullable=False)
 
     def __init__(self, date):
-        self._first_date = date
-        self._latest_date = date
-
-    @hybrid_property
-    def first_date(self):
-        return self._first_date
-
-    @hybrid_property
-    def latest_date(self):
-        return self._latest_date
-
-    @latest_date.setter
-    def latest_date(self, value):
-        self._latest_date = value
+        self.first_date = date
+        self.latest_date = date
 
     def __repr__(self):
-        return f"DbInfo(first_date={self.first_date}, " \
-               f"latest_date={self.latest_date})"
+        return f"DbInfo(first_date={self.first_date}, latest_date={self.latest_date})"
 
 
 def get_dbinfo():
