f057bf9b04811d61a701168971993c27fc98d190
David Sanders <david.wm.sanders@gmail.com>
2020-02-17 07:13:37 +0000

    Polish rwrtrack/exceptions.py

--- rwrtrack/exceptions.py
+++ rwrtrack/exceptions.py
@@ -1,14 +1,25 @@
-class NoAccountError(Exception):
+class RwrtrackException(Exception):
+    """Base class for exceptions in the rwrtrack module."""
     pass
 
-class NoRecordError(Exception):
+# Exceptions for ORM-mapped database tables
+class NoAccountError(RwrtrackException):
+    """Raised when no 'accounts WHERE filters' in database."""
     pass
 
-class FilterOperatorError(Exception):
+class NoRecordError(RwrtrackException):
+    """Raised when no 'records WHERE filters' in database."""
     pass
 
-class FilterMetricError(Exception):
+# Exceptions for the filtering system in filter.py
+class FilterOperatorError(RwrtrackException):
+    """Raised when the operator in a filter string is unhandled."""
     pass
 
-class FilterValueError(Exception):
+class FilterMetricError(RwrtrackException):
+    """Raised when the metric in a filter string does not exist."""
+    pass
+
+class FilterValueError(RwrtrackException):
+    """Raised when the value in a filter string can not be converted to a float."""
     pass
